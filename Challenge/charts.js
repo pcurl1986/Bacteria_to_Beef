2da1e776c9711c75e564514c9cd6bad50f0e2b24
pcurl1986 <101307058+pcurl1986@users.noreply.github.com>
2022-07-19 17:52:05 -0700

    Update charts.js

--- Challenge/charts.js
+++ Challenge/charts.js
@@ -1,9 +1,28 @@
+function init() {
+  // Grab a reference to the dropdown select element
+  var selector = d3.select("#selDataset");
 
+  // Use the list of sample names to populate the select options
+  d3.json("samples.json").then((data) => {
+    var sampleNames = data.names;
 
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
 
+    // Use the first sample from the list to build the initial plots
+    var firstSample = sampleNames[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+  });
+}
 
+// Initialize the dashboard
+init();
 
-<<<<<<< HEAD
 function optionChanged(newSample) {
   // Fetch new data each time a new sample is selected
   buildMetadata(newSample);
